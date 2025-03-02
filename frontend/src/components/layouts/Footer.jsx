e002f20a38bf6f271c70482d3478381fbfe67eff
abc <shrihari.mane22@it.sce.edu.in>
2025-03-02 21:56:48 +0530

    Initial commit

---
+++ frontend/src/components/layouts/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <>
+    <footer>
+<p className="text-center">
+    MEDICINE  Delivery website-2024-25. All Copy Rights Reserved by SHRIHARI 
+</p>
+    </footer>
+    </>
+  );
+}
